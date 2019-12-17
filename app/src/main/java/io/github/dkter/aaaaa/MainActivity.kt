f00e85e9fac4843e2cfb84efa1f932704ca967ed
David Teresi <dkteresi+github@gmail.com>
2019-12-16 21:07:52 -0500

    add a splash screen with a link to enable the keyboard in settings
    i love writing kotlin and java in the same android app

--- app/src/main/java/io/github/dkter/aaaaa/MainActivity.kt
+++ app/src/main/java/io/github/dkter/aaaaa/MainActivity.kt
@@ -1,12 +1,25 @@
 package io.github.dkter.aaaaa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun keyboardSettings(v: View) {
+        val intent = Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS)
+        startActivity(intent)
+    }
+
+    fun closeKeyboardSettingsReminder(v: View) {
+        val toast = Toast.makeText(this, "Feature not implemented", Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
