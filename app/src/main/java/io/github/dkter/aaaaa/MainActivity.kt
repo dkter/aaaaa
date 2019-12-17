6c42771f00bd67de8000f93c430b48acda86bfb4
David Teresi <dkteresi+github@gmail.com>
2019-12-16 21:58:36 -0500

    Add input validation for text box

--- app/src/main/java/io/github/dkter/aaaaa/MainActivity.kt
+++ app/src/main/java/io/github/dkter/aaaaa/MainActivity.kt
@@ -3,14 +3,21 @@ package io.github.dkter.aaaaa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testBox = findViewById<EditText>(R.id.testBox);
+        testBox.addTextChangedListener(this);
     }
 
     fun keyboardSettings(v: View) {
@@ -21,5 +28,26 @@ class MainActivity : AppCompatActivity() {
     fun closeKeyboardSettingsReminder(v: View) {
         val toast = Toast.makeText(this, "Feature not implemented", Toast.LENGTH_SHORT)
         toast.show()
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun afterTextChanged(editable: Editable) {
+
+    }
+
+    override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        if (text != null) {
+            val withoutA = text.replace("a".toRegex(), "")
+            val errorField = findViewById<TextView>(R.id.testBoxErrorField)
+            if (!withoutA.isBlank()) {
+                errorField.text = "Error: invalid characters detected"
+            }
+            else {
+                errorField.text = ""
+            }
+        }
     }
 }
